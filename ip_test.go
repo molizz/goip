eeb00712cbba0b473916da573de549f14a2c9ace
moli <molix2@outlook.com>
2021-01-11 16:55:47 +0800

    add 腾讯ip测试

--- ip_test.go
+++ ip_test.go
@@ -14,6 +14,16 @@ func TestGetLocation(t *testing.T) {
 	fmt.Println(location.ToString())
 }
 
+func TestTencentLocation(t *testing.T) {
+	location, err := AddTencent("Y2ABZ-ENY3I-UBNG7-5FNFZ-SZENZ-2ZFTX").GetLocation("123.58.180.8", 60)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("国: %s 省: %s 城: %s", location.GetCountry(), location.GetRegion(), location.GetCity())
+	fmt.Println(location.ToString())
+}
+
 func TestGetLocationFreeIPIP(t *testing.T) {
 	location, err := AddIPIP("token").GetLocation("123.58.180.8", 60)
 	if err != nil {
